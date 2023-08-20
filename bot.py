3ef3f4f10ec74cd3641e9d7dc44d9001b0b9341c
Antonio Ponce <zponce@posteo.net>
2023-08-19 23:11:11 -0400

    got spreadsheet data, added a bunch of functions...

--- bot.py
+++ bot.py
@@ -1,10 +1,24 @@
+import os 
 import discord
-from discord.ext import commands
 import responses
+from datetime import date
+from dotenv import load_dotenv
+from discord.ext import commands
+# Custom 
+import ss 
 
-# Main Token 
-# This Token shouldn't live here. Please move this into a .env file
-TOKEN = ''
+# Bot Token 
+TOKEN = os.environ.get('TOKEN')
+
+# Other Variables
+key_path = os.environ.get('KEYPATH')
+worksheet_name = os.environ.get('WORKSHEET')
+sheet_name = os.environ.get('SHEET')
+channel1 = int(os.environ.get('CHANNEL1'))
+channel2 = int(os.environ.get('CHANNEL2'))
+channel3 = int(os.environ.get('CHANNEL3'))
+
+# Auxiliaries
 
 async def send_message(msg, user_msg, is_private):
 
@@ -20,40 +34,87 @@ async def send_message(msg, user_msg, is_private):
     except Exception as e: 
         print(e)
 
-def run_bot():
-    client = discord.Client(intents=discord.Intents.default())
+async def send_to_channel(channel_num, message, bot):
+    ch = bot.get_channel(channel_num)
+    await ch.send(message)
 
-    @client.event
-    async def on_ready():
+## Spreadsheet stuff
+def format_item(item):
 
-        print(f'{client.user} is now running ')
+    # Note: This is too trusting. Need to come up with a better way to get specific items 
+    # from item array....
+    formated_item = {
+        "DATE" : date.today(),
+        "IMAGE" : item[2],
+        "NAME" : item[3],
+        "ASIN" : item[4],
+        "AMZLINK" : item[5],
+        "LINK": item[6],
+        "COST" : item[7],
+        "SALEPRICE": item[8],
+        "PROFIT" : item[9],
+        "ROI" : item[10]
+    }
 
+    return formated_item
+         
+def format_sheet_data(sheet_data):
     
-    @client.event 
-    async def on_message(msg):
-        if msg.author == client.user: 
-            return 
+    result = None
+
+    if(len(sheet_data) > 1):
         
-        username = str(msg.author)
-        user_msg = str(msg.content)
-        channel = str(msg.channel)
+        payload = map(format_item, sheet_data)
+        result = list(payload)
+    else:
+        result = False
+
+    return result 
 
 
-        print(f'User {username} said \'{user_msg}\' in {channel}')
+def write_message(item):
+    msg1 = f'[{item["NAME"]}]({item["AMZLINK"]})'
+    msg2 = f'\n**Buy Here:**\n[View Product]({item["LINK"]})'
+    msg3 = f'\n**ASIN:**\n{item["ASIN"]}'
+    msg4 = f'\n**Cost:**\n{item["COST"]}'
+    msg5 = f'\n**Sale Price:**\n{item["SALEPRICE"]}'
+    msg6 = f'\n**Profit|ROI:**\n{item["PROFIT"]}|${item["ROI"]}'
 
-        # if(user_msg[0] == '?'):
-        #     user_message = user_msg[1:]
-        #     await send_message(msg, user_message, is_private=True)
-        # else:
-        #     await send_message(msg, user_message, is_private=False)
-        print(msg)
-        print(f'USER MESSAGE: {user_msg}')
+    return msg1 + msg2 + msg3 + msg4 + msg5 + msg6
+
+
+## Main Functions
+
+async def send_spreadsheet_data(bot):
+
+    # Get spreadsheet values
+    spread_sheet_raw = ss.open_worksheet(key_path, worksheet_name, sheet_name)
+    # Get raw spreadsheet values | Note: val should be generated... not hard coded
+    spread_sheet_vals = ss.get_worksheet_values("A5:K7", spread_sheet_raw)
+    # Get formatted values
+    formatted_values = format_sheet_data(spread_sheet_vals)
+    # Once you have your values, send messages based on count...
+    count = 0
+
+    for formattedItem in formatted_values:
+
+        message = write_message(formattedItem)
+        
+        channel_number = channel1
+
+        if(count == 1):
+            channel_number = channel2
+        elif(count > 1):
+            channel_number = channel3
+
+        await send_to_channel(channel_number, message, bot)
+
+        count += 1
     
-    # Everything must be declared before this event
-    client.run(TOKEN)
+    print("OK| Messages Sent!")
 
 
-def run_neo_bot():
+def run_bot():
 
     intents = discord.Intents.default()
     intents.message_content = True
@@ -63,6 +124,8 @@ def run_neo_bot():
     async def on_ready():
         print(f'Andromeda is running...')
         print('------')
+        print('Attempting to send messages.....')
+        await send_spreadsheet_data(bot)
 
     @bot.command()
     async def greeting(ctx):
@@ -81,6 +144,14 @@ def run_neo_bot():
     @bot.command()
     async def hello(ctx):
         await ctx.send('I exist to serve the will of Pontius')
+
+
+    # ctx = context
+    # @bot.command()
+    # async def ping(ctx):
+    #     # It's an interger
+    #     channel = bot.get_channel(1141489875124752524)
+    #     await channel.send('Hello Channel!')
 
 
     bot.run(TOKEN)
--- todo.md
+++ todo.md
@@ -1,6 +1,3 @@
 ## Todo list 
 * find scheduling for bot (run at x time)
-* research google API for sheets 
-* create functionality to get sheet data 
-* create functionality to parse through sheet data 
-* create functionality to dump parsed data into discord channel 
+* Create embedded message for formatted information
